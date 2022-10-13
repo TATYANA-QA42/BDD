e59300de8a5b3781f0b1cd7273580d727edc5738
Татьяна Пислегина <ztv-1988@yandex.ru>
2022-10-14 00:30:01 +0500

    pro

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -1,6 +1,81 @@
 package ru.netology.web.data;
-import com.codeborne.selenide.Condition;
-import static com.codeborne.selenide.Selenide.$;
-public class DashboardPage {
+//import lombok.Value;
+//
+//import static com.codeborne.selenide.Selenide.$;
+//public class DataHelper {
+//    private DataHelper () {
+//    }
+//@Value
+//    public static class AuthInfo {
+//    public String login;
+//    public String password;
+//}
+//public static AuthInfo getAuthInfo () {
+//        return new AuthInfo("vasya", "qwerty123");
+//}
+////public static  AuthInfo getOtherAuthInfo(AuthInfo origin) {
+//   // return new AuthInfo("vasya", "qwerty123");
+////}
+//    @Value
+//public static class VerificationCode {
+//    public String code;
+//
+//        }
+//
+//        public static  VerificationCode getVerificationCodeFor(AuthInfo authInfo){
+//        return new VerificationCode("12345");
+//}
+//    @Value
+//    public static class CardData {
+//        public String number;
+//    }
+//    public static  CardData getFirstCardNumber() {
+//        return new CardData("5559 0000 0000 0001");
+//    }
+//    public static CardData getSecondCardNumber () {
+//        return new CardData ("5559 0000 0000 0002");
+//    }
+//}
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+        return new AuthInfo("petya", "123qwerty");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class CardData {
+        private String number;
+    }
+
+    public static CardData getFirstCardNumber() {
+        return new CardData("5559 0000 0000 0001");
+    }
+
+    public static CardData getSecondCardNumber() {
+        return new CardData("5559 0000 0000 0002");
+    }
 
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class MoneyTransferTest {
-}
+package ru.netology.web.test;
+
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+import ru.netology.web.page.TransferPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+    @BeforeEach
+    void shouldLogin() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneySecondToFirstCard() {
+        int value = 100;
+        String cardNumber = String.valueOf(DataHelper.getSecondCardNumber());
+        val dashboardPage = new DashboardPage();
+        var firstCardBalance = dashboardPage.getFirstCardBalance();
+        var secondCardBalance = dashboardPage.getSecondCardBalance();
+        dashboardPage.transferButtonSecondToFirst();
+        val transferPage = new TransferPage();
+        transferPage.importTransferDate(value, cardNumber);
+        var firstCardBalance1 = dashboardPage.getFirstCardBalance();
+        var secondCardBalance1 = dashboardPage.getSecondCardBalance();
+        Assertions.assertEquals(secondCardBalance - value, secondCardBalance1);
+        Assertions.assertEquals(firstCardBalance + value, firstCardBalance1);
+    }
+
+    @Test
+    void shouldTransferMoneyFirstToSecondCard() {
+        int value = 100;
+        String cardNumber = String.valueOf(DataHelper.getFirstCardNumber());
+        val dashboardPage = new DashboardPage();
+        var firstCardBalance = dashboardPage.getFirstCardBalance();
+        var secondCardBalance = dashboardPage.getSecondCardBalance();
+        dashboardPage.transferButtonFirstToSecond();
+        val transferPage = new TransferPage();
+        transferPage.importTransferDate(value, cardNumber);
+        var firstCardBalance1 = dashboardPage.getFirstCardBalance();
+        var secondCardBalance1 = dashboardPage.getSecondCardBalance();
+        Assertions.assertEquals(firstCardBalance - value, firstCardBalance1);
+        Assertions.assertEquals(secondCardBalance + value, secondCardBalance1);
+    }
+    @Test
+    void doNotShouldTransferMoneyFirstToSecondCardAfterLimit()
+    {
+        int value = 100;
+        String cardNumber = String.valueOf(DataHelper.getSecondCardNumber());
+        val dashboardPage = new DashboardPage();
+        var secondCardBalance = dashboardPage.getSecondCardBalance();
+        dashboardPage.transferButtonSecondToFirst();
+        val transferPage = new TransferPage();
+        transferPage.importTransferDate(value+secondCardBalance,cardNumber);
+        transferPage.getNotification();
+    }}
+
+
