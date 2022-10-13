42718e80b7a3ccd9fabc45156e3981ca9835b9ec
Татьяна Пислегина <ztv-1988@yandex.ru>
2022-10-14 00:25:24 +0500

    ignore

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,6 @@
+package ru.netology.web.data;
+import com.codeborne.selenide.Condition;
+import static com.codeborne.selenide.Selenide.$;
+public class DashboardPage {
+
+}
---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,3 @@
+package ru.netology.web.data;
+public class DashboardPage {
+}
